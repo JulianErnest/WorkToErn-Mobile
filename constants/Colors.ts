8d64f8b4c11c83be8c7388330676f867139b3428
JulianErnest <76834365+JulianErnest@users.noreply.github.com>
2023-05-22 22:05:29 +0800

    feat: added common components

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -4,14 +4,14 @@ const tintColorDark = '#fff';
 export default {
   light: {
     text: '#000',
-    background: '#fff',
+    background: '#4A65AD',
     tint: tintColorLight,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorLight,
   },
   dark: {
     text: '#fff',
-    background: '#000',
+    background: '#4A65AD',
     tint: tintColorDark,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
